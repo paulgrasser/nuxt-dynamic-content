2b951f7fca3e761333090f47e2d2167a0f96315f
paulgrasser <50242881+paulgrasser@users.noreply.github.com>
2020-06-11 13:20:22 +0200

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-alert :hidden="error" type="error">Yes</v-alert>
+    <v-btn @click="error = !error">Yee</v-btn>
+  </div>  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    error: false
+  })
+}
+</script>
